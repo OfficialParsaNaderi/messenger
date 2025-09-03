5a4f8ab32b3bc95e849e687156f068e756956a73
OfficialParsaNaderi <parsanaderi1387.programmer@gmail.com>
2025-09-03 14:04:00 +0330

    Messenger

---
+++ prisma/migrations/20250903102633_connection/migration.sql
@@ -0,0 +1,31 @@
+/*
+  Warnings:
+
+  - You are about to drop the `_ConversationToUser` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "public"."_ConversationToUser" DROP CONSTRAINT "_ConversationToUser_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."_ConversationToUser" DROP CONSTRAINT "_ConversationToUser_B_fkey";
+
+-- DropTable
+DROP TABLE "public"."_ConversationToUser";
+
+-- CreateTable
+CREATE TABLE "public"."_UserConversations" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+
+    CONSTRAINT "_UserConversations_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE INDEX "_UserConversations_B_index" ON "public"."_UserConversations"("B");
+
+-- AddForeignKey
+ALTER TABLE "public"."_UserConversations" ADD CONSTRAINT "_UserConversations_A_fkey" FOREIGN KEY ("A") REFERENCES "public"."Conversation"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."_UserConversations" ADD CONSTRAINT "_UserConversations_B_fkey" FOREIGN KEY ("B") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
